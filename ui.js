3e64341bc95354476818c60530591882fdf92dca
Artem <duraek@gmail.com>
2022-01-15 17:08:53 -0500

    Added "Price" input
    Added "Price" input.

--- ui.js
+++ ui.js
@@ -5,7 +5,7 @@ var currency;
 var known_prices = {};
 var exchangerate_ready = false;
 const sleep_time = 250;
-const delete_times = 15;
+const delete_times = 15;    
 
 async function get_exchangerate() {
     const result = await $.ajax({
@@ -19,12 +19,52 @@ async function get_exchangerate() {
     return (currency = result);
 }
 
+
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+
 function hasClass(elem, className) {
     return elem.className.split(" ").indexOf(className) > -1;
+}
+
+
+function edit_product(product, price_cad) {
+    //input top price
+    $(product).find('[name="item_price"]').click();
+    for (let index = 0; index < delete_times; index++) {
+        $(`.numpad`).find('[data-key="del"]').click();
+    }
+    for (var i = 0; i < price_cad.length; i++) {
+        $(`.numpad`).find(`[data-key="${price_cad.charAt(i)}"]`).click();
+    }
+    $('[data-key="ret"]').click();
+    //end
+
+    //input bottom price
+    if (product.querySelector("#regular_price") == undefined) {
+        $(product).find('[data-action="more"]').children()[0].click();
+    }
+
+    $('[name="regular_price"]').click();
+    for (let index = 0; index < delete_times; index++) {
+        $(`.numpad`).find('[data-key="del"]').click();
+    }
+    for (var i = 0; i < price_cad.length; i++) {
+        $(`.numpad`).find(`[data-key="${price_cad.charAt(i)}"]`).click();
+    }
+    $('[data-key="ret"]').click();
+    $(product).find('[data-action="more"]').children()[0].click();
+    //end
+
+    //Indicate CAD price in Total
+    $(product).find('.total').text(`CAD $${price_cad}`);
+    //end
+
+    //Indicate CAD price in Price
+    product.querySelectorAll('.price')[1].children[0].value = price_cad;
+    //end
 }
 
 async function create_button() {
@@ -45,6 +85,7 @@ async function create_button() {
             }
             if (cart.length > 0) {
                 if (cart[0].querySelector(".canadian-price") == undefined) {
+                    //add button
                     const span = document.createElement("button");
                     span.classList.add("btn");
                     span.classList.add("btn-success");
@@ -54,8 +95,32 @@ async function create_button() {
                     const a = document.createElement("a");
                     a.appendChild(span);
                     cart[0].appendChild(a);
+                    //end
+
+                    //add input
+                    $(".cart-header > .price").clone().insertAfter('.cart-header > .price');
+                    document.querySelector(".cart-header").querySelector(".price").innerText = "Discount";
+                    //end
                 }
             }
+
+            var cart = document.querySelector(".panel-body.list.cart-list").children[0].children;
+            if (!hasClass(cart[0], "empty")) {
+                for (const product of cart) {
+                    if (product.querySelectorAll(".price").length == 1) {
+                        $(product).find(".price").clone().insertAfter($(product).find(".price"));
+                        product.querySelectorAll(".price")[1].innerHTML = `<input type="text" name="item_price_cad" class="form-control autogrow" style="width: 63px;">`;
+                        product.querySelectorAll(".price")[1].addEventListener('keypress', function (e) {
+                            if (e.key === 'Enter') {
+                                const product = e.path[3];
+                                const price_cad = e.path[0].value;
+                                edit_product(product, price_cad)
+                            }
+                        });
+                    }
+                }
+            }
+
             await sleep(sleep_time);
         } catch (error) {
             console.log(error);
@@ -72,7 +137,6 @@ window.onload = async function () {
         async function (e) {
             if (hasClass(e.target, "canadian-price")) {
                 for (const product of document.querySelector(".panel-body.list.cart-list").children[0].children) {
-
                     //get price
                     const title = $(product).find('[data-name="title"]').text();
                     var price_usd_check = 0;
@@ -86,41 +150,13 @@ window.onload = async function () {
 
                     }
                     const price_cad = known_prices[title];
-                    if (price_cad > 0 && price_usd_check == 0 && $(product).find('.total').text().includes('CAD')) {
+
+                    if ($(product).find('.total').text().includes('CAD') || $(product).find('.total').text().split("$").length == 3) {
                         continue;
                     }
                     //end
 
-                    //input top price
-                    $(product).find('[name="item_price"]').click();
-                    for (let index = 0; index < delete_times; index++) {
-                        $(`.numpad`).find('[data-key="del"]').click();
-                    }
-                    for (var i = 0; i < price_cad.length; i++) {
-                        $(`.numpad`).find(`[data-key="${price_cad.charAt(i)}"]`).click();
-                    }
-                    $('[data-key="ret"]').click();
-                    //end
-
-                    //input bottom price
-                    if (product.querySelector("#regular_price") == undefined) {
-                        $(product).find('[data-action="more"]').children()[0].click();
-                    }
-
-                    $('[name="regular_price"]').click();
-                    for (let index = 0; index < delete_times; index++) {
-                        $(`.numpad`).find('[data-key="del"]').click();
-                    }
-                    for (var i = 0; i < price_cad.length; i++) {
-                        $(`.numpad`).find(`[data-key="${price_cad.charAt(i)}"]`).click();
-                    }
-                    $('[data-key="ret"]').click();
-                    $(product).find('[data-action="more"]').children()[0].click();
-                    //end
-
-                    //Indicate CAD price
-                    $(product).find('.total').text(`CAD $${price_cad}`);
-                    //end
+                    edit_product(product, price_cad)
                 }
             }
         },
